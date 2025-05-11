70d2eb556a90901db244f963d636880ad8032b11
Akarwshn <akaraushan11@gmail.com>
2025-05-12 02:03:39 +0700

    Delete js/kirim-wish.js

--- js/kirim-wish.js
+++
@@ -1,5 +0,0 @@
-fetch("https://akarwshan.my.id/simpan_ucapan.php", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ nama: "Andi", ucapan: "Selamat menempuh hidup baru!" })
-});
